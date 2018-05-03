fca1b5f5e7b6505628407736a0314876ecaf548d
JoeSvr95 <joesaverio@hotmail.com>
2018-05-03 01:39:31 -0500

    Commit del juego

---
+++ Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour {
+	public void LoadNextLevel(){
+		/* Inicia el siguiente nivel acorde al indice en el build
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1); */
+		Debug.Log ("Siguiente Nivel!");
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float fowardForce = 2000f;
+	public float sidewaysForce = 500f;
+	
+	// Es mejor utilizar FixedUpdate en vez de Update para la física del juego.
+	void FixedUpdate () {
+		rb.AddForce (0, 0, fowardForce * Time.deltaTime); // Time.deltaTime compensa los FPS para todos los sistemas.
+
+		if (Input.GetKey("d")){
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey("a")){
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (rb.position.y < -1f) {
+			FindObjectOfType<GameManager> ().EndGame ();
+		}
+	}
+}
